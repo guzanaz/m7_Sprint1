de47221b391d202b0db209423c4aec0daf0b140f
Daniela <daniela.gallardo.reyes@gmail.com>
2021-11-29 16:28:27 +0100

    Create virtual_machine.php
    corrección de modelo

---
+++ app/Models/virtual_machine.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class virtual_machine extends Model
+{
+    use HasFactory;
+    //modifica la table virtual_machine
+    protected $table='virtual_machine';
+
+
+}
