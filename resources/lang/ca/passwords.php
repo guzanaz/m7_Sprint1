d006998206745da0b4ed80279c569db3e36e0520
Daniela <daniela.gallardo.reyes@gmail.com>
2022-01-10 16:27:17 +0100

    translate to catalá some notifications
    all need to be re-checked

---
+++ resources/lang/ca/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contrasenya ha de tenir un mínim de 6 caràcters i estar correctament repetida.',
+    'reset' => 'S\'ha reiniciat la teva contrasenya!',
+    'sent' => 'T\'hem enviat per e-mail un enllaç per a reiniciar la teva contrasenya!',
+    'throttled' => 'Espereu abans de tornar-ho a provar.',
+    'token' => 'El testimoni de reinici de contrasenya no és correcte.',
+    'user' => "No existeix cap usuari amb aquest e-mail.",
+
+];
