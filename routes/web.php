bfd6c2664dd89f019064847ec54444e5e604d536
Daniela <daniela.gallardo.reyes@gmail.com>
2022-02-11 17:37:33 +0100

    eliminar rutas web.php

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,3 @@ use App\Http\Controllers\VirtualMachineController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('StudyProgram',[StudyProgramController::class,'index'])->name('StudyProgram.index');
-Route::get('StudyProgram/create',[StudyProgramController::class,'create'])->name('StudyProgram.create');
-Route::post('StudyProgram', [StudyProgramController::class,'store'])->name('StudyProgram.store');
-Route::get('StudyProgram/{program}',[StudyProgramController::class,'show'])->name('StudyProgram.show');
-Route::get('StudyProgram/{program}/edit',[StudyProgramController::class,'edit'])->name('StudyProgram.edit');
-Route::put('StudyProgram/{program}', [StudyProgramController::class,'update']) ->name('StudyProgram.update');
-Route::delete('StudyProgram{program}',[StudyProgramController::class,'destroy']) ->name('StudyProgram.destroy');
-
-Route::get('VirtualMachine',[VirtualMachineController::class,'index'])->name('VirtualMachine.index');
-Route::get('VirtualMachine/create',[VirtualMachineController::class,'create'])->name('VirtualMachine.create');
-Route::post('VirtualMachine', [VirtualMachineController::class,'store'])->name('VirtualMachine.store');
-Route::get('VirtualMachine/show/{virtualMachine}',[VirtualMachineController::class,'show'])->name('VirtualMachine.show');
-Route::get('VirtualMachine/{virtualMachine}/edit',[VirtualMachineController::class,'edit'])->name('VirtualMachine.edit');
-Route::put('VirtualMachine/{virtualMachine}', [VirtualMachineController::class,'update'])->name('VirtualMachine.update');
-Route::delete('VirtualMachine{virtualMachine}',[VirtualMachineController::class,'destroy'])->name('VirtualMachine.destroy');
